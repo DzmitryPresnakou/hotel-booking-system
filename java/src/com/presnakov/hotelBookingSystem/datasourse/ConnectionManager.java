d81e06a45107499c350ce6d5eca71eb06ea6d904
DzmitryPresnakou <7745834@gmail.com>
2024-04-25 18:12:04 +0300

    added lombok commit

--- java/src/com/presnakov/hotelBookingSystem/datasourse/ConnectionManager.java
+++ java/src/com/presnakov/hotelBookingSystem/datasourse/ConnectionManager.java
@@ -1,5 +1,8 @@
 package com.presnakov.hotelBookingSystem.datasourse;
 
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,6 +12,7 @@ import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+@UtilityClass
 public final class ConnectionManager {
 
     private static final String PASSWORD_KEY = "db.password";
@@ -25,16 +29,13 @@ public final class ConnectionManager {
         initConnectionPool();
     }
 
-    private ConnectionManager() {
-    }
-
     private static void initConnectionPool() {
         var poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
         var size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
         pool = new ArrayBlockingQueue<>(size);
         sourceConnections = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            var connection = open();
+            var connection = get();
             var proxyConnection = (Connection)
                     Proxy.newProxyInstance(ConnectionManager.class.getClassLoader(), new Class[]{Connection.class},
                             (proxy, method, args) -> method.getName().equals("close")
@@ -45,24 +46,12 @@ public final class ConnectionManager {
         }
     }
 
+    @SneakyThrows
     public static Connection get() {
-        try {
-            return pool.take();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Connection open() {
-        try {
-            return DriverManager.getConnection(
-                    PropertiesUtil.get(URL_KEY),
-                    PropertiesUtil.get(USERNAME_KEY),
-                    PropertiesUtil.get(PASSWORD_KEY)
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return DriverManager.getConnection(
+                PropertiesUtil.get(URL_KEY),
+                PropertiesUtil.get(USERNAME_KEY),
+                PropertiesUtil.get(PASSWORD_KEY));
     }
 
     private static void loadDriver() {
--- java/src/com/presnakov/hotelBookingSystem/service/RoomService.java
+++ java/src/com/presnakov/hotelBookingSystem/service/RoomService.java
@@ -18,13 +18,14 @@ public class RoomService {
 
     public List<RoomDto> findAll() {
         return roomDao.findAll().stream()
-                .map(room -> new RoomDto(
-                        room.getId(),
-                        """
-                                beds: %s - class: %s - status: %s - hotel: %s
-                                 """.formatted(room.getRoomOccupancy(), room.getRoomClass().getComfortClass(),
-                                room.getRoomStatus().getRoomStatusEnum(), room.getHotel().getName())
-                ))
+                .map(room -> RoomDto.builder()
+                        .id(room.getId())
+                        .description(
+                                """
+                                        beds: %s - class: %s - status: %s - hotel: %s
+                                        """.formatted(room.getRoomOccupancy(), room.getRoomClass().getComfortClass(),
+                                        room.getRoomStatus().getRoomStatusEnum(), room.getHotel().getName()))
+                        .build())
                 .collect(toList());
     }
 
